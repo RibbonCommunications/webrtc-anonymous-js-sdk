91f96f3827bdc0796d49ceef605c1bbbac59bfd6
machine <janwa039@gmail.com>
2024-10-25 16:32:48 +0000

    1471

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ To install a particular build branch or beta branch with the Github URL, run:
 ### In the browser :
 ```html
 <script src="https://unpkg.com/@rbbn/webrtc-anonymous-js-sdk@6.16.0/dist/webrtc.js"
-  integrity="sha384-77qNfL2pJUnn57YPqIwO7DHEc57VrPDn52oDtuH4lMXlD2DEBYdU6hxmm3WjNb+T"
+  integrity="sha384-Vy1Xd4zsTp330yEx2H6puX0rtRECVenKaF5F6JZ72HqndLNo6HTVsEgJNbgfmtQg"
   crossorigin="anonymous">
 </script>
 ```
--- dist/webrtc.js
+++ dist/webrtc.js
@@ -12,7 +12,7 @@
  *
  * WebRTC.js
  * webrtc.anonymous.js
- * Version: 6.16.0-beta.1469
+ * Version: 6.16.0-beta.1471
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -2348,7 +2348,7 @@ module.exports = root;
 
 /***/ }),
 
-/***/ 79761:
+/***/ 15740:
 /***/ ((__unused_webpack_module, exports) => {
 
 "use strict";
@@ -2366,7 +2366,7 @@ exports.getVersion = getVersion;
  * for the @@ tag below with actual version value.
  */
 function getVersion() {
-  return '6.16.0-beta.1469';
+  return '6.16.0-beta.1471';
 }
 
 /***/ }),
@@ -11136,7 +11136,7 @@ Object.defineProperty(exports, "__esModule", ({
 exports["default"] = getStatsOperation;
 var _selectors = __webpack_require__(40481);
 var _kandyWebrtc = __webpack_require__(37654);
-var _version = __webpack_require__(79761);
+var _version = __webpack_require__(15740);
 var _sdkId = _interopRequireDefault(__webpack_require__(20855));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
 // Call plugin.
@@ -23842,7 +23842,7 @@ __webpack_require__(91883);
 __webpack_require__(70286);
 var _logs = __webpack_require__(69932);
 var _utils = __webpack_require__(1011);
-var _version = __webpack_require__(79761);
+var _version = __webpack_require__(15740);
 var _defaults = __webpack_require__(24679);
 var _validation = __webpack_require__(52932);
 // Other plugins.
@@ -35299,7 +35299,7 @@ var _reduxSaga = _interopRequireDefault(__webpack_require__(71028));
 var _effects = __webpack_require__(89979);
 var _bottlejs = _interopRequireDefault(__webpack_require__(8997));
 var _utils = __webpack_require__(1011);
-var _version = __webpack_require__(79761);
+var _version = __webpack_require__(15740);
 var _intervalFactory = _interopRequireDefault(__webpack_require__(73181));
 var _validation = __webpack_require__(52932);
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
@@ -39407,7 +39407,7 @@ var _cloneDeep2 = _interopRequireDefault(__webpack_require__(89321));
 var _selectors = __webpack_require__(45590);
 var _selectors2 = __webpack_require__(87075);
 var _logs = __webpack_require__(69932);
-var _version = __webpack_require__(79761);
+var _version = __webpack_require__(15740);
 var _utils = __webpack_require__(1011);
 var _effects = __webpack_require__(89979);
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { default: e }; }
@@ -77697,7 +77697,7 @@ module.exports = str => encodeURIComponent(str).replace(/[!'()*]/g, x => `%${x.c
 
 /***/ }),
 
-/***/ 96347:
+/***/ 98610:
 /***/ ((__unused_webpack_module, exports, __webpack_require__) => {
 
 "use strict";
@@ -78138,7 +78138,7 @@ var _v4 = _interopRequireDefault(__webpack_require__(93423));
 
 var _nil = _interopRequireDefault(__webpack_require__(35911));
 
-var _version = _interopRequireDefault(__webpack_require__(96347));
+var _version = _interopRequireDefault(__webpack_require__(98610));
 
 var _validate = _interopRequireDefault(__webpack_require__(4564));
 
@@ -86074,7 +86074,7 @@ module.exports = function (key, value) {
 
 var globalThis = __webpack_require__(79117);
 var fails = __webpack_require__(5234);
-var V8 = __webpack_require__(80319);
+var V8 = __webpack_require__(47842);
 var ENVIRONMENT = __webpack_require__(11078);
 
 var structuredClone = globalThis.structuredClone;
@@ -86097,7 +86097,7 @@ module.exports = !!structuredClone && !fails(function () {
 "use strict";
 
 /* eslint-disable es/no-symbol -- required for testing */
-var V8_VERSION = __webpack_require__(80319);
+var V8_VERSION = __webpack_require__(47842);
 var fails = __webpack_require__(5234);
 var globalThis = __webpack_require__(79117);
 
@@ -87082,10 +87082,10 @@ var fails = __webpack_require__(5234);
 var aCallable = __webpack_require__(44977);
 var internalSort = __webpack_require__(9295);
 var ArrayBufferViewCore = __webpack_require__(47223);
-var FF = __webpack_require__(54509);
+var FF = __webpack_require__(57964);
 var IE_OR_EDGE = __webpack_require__(84598);
-var V8 = __webpack_require__(80319);
-var WEBKIT = __webpack_require__(41719);
+var V8 = __webpack_require__(47842);
+var WEBKIT = __webpack_require__(41374);
 
 var aTypedArray = ArrayBufferViewCore.aTypedArray;
 var exportTypedArrayMethod = ArrayBufferViewCore.exportTypedArrayMethod;
@@ -87433,7 +87433,7 @@ if (DESCRIPTORS && !('size' in URLSearchParamsPrototype)) {
 
 /***/ }),
 
-/***/ 54509:
+/***/ 57964:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 "use strict";
@@ -87447,7 +87447,7 @@ module.exports = !!firefox && +firefox[1];
 
 /***/ }),
 
-/***/ 80319:
+/***/ 47842:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 "use strict";
@@ -87483,7 +87483,7 @@ module.exports = version;
 
 /***/ }),
 
-/***/ 41719:
+/***/ 41374:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
 "use strict";
